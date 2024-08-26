7987d39ed54cbb342bb66acec26852b54845dc1a
Joao Vitor Diamon Roldan dos Santos <143654414+JoaoVitorDiamon@users.noreply.github.com>
2024-08-26 09:17:25 -0300

    Update Login.tsx

--- src/Login.tsx
+++ src/Login.tsx
@@ -17,163 +17,132 @@ const loginFormValidationSchema = zod.object({
 type NewLoginFormData = zod.infer<typeof loginFormValidationSchema>;
 
 const Login = () => {
-
-  const [inputPasswordType, setInputPasswordType] =
-    useState<PasswordType>("password");
+  const [inputPasswordType, setInputPasswordType] = useState<PasswordType>("password");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const handleTogglePasswordType = (type: PasswordType) => {
-    switch (type) {
-      case "password":
-        setInputPasswordType("text");
-        return;
-      case "text":
-      default:
-        setInputPasswordType("password");
-        return;
-    }
+    setInputPasswordType(type === "password" ? "text" : "password");
   };
-
-
-
-
-  type NewLoginFormData = zod.infer<typeof loginFormValidationSchema>;
 
   const loginForm = useForm<NewLoginFormData>({
     resolver: zodResolver(loginFormValidationSchema),
   });
 
   const { register, handleSubmit, formState, reset } = loginForm;
-
   const { errors } = formState;
 
   const handleLoginSubmit = async (data: NewLoginFormData) => {
     try {
-      const url = `https://2ee7-189-29-146-118.ngrok-free.app/Users/login/email=${data.email}&&password=${data.senha}`;
+      const url = `https://1705-189-29-146-118.ngrok-free.app/Users/login/email=${data.email}&&password=${data.senha}`;
       const response = await axios.get(url, {
         headers: {
           'ngrok-skip-browser-warning': 'true'
         },
       });
-      if(response.data == 'Email ou senha incorretos'){
+      
+      if (response.data === 'Email ou senha incorretos') {
+        setErrorMessage("Email ou senha incorretos");
         reset();
-      }else{
-        const {token, username} = response.data;
-        localStorage.setItem('sessionToken', token)
-        localStorage.setItem('username', username)
-              window.location.href = '/'
+      } else {
+        const { token, username, Id } = response.data;
+        localStorage.setItem('sessionToken', token);
+        localStorage.setItem('username', username);
+        localStorage.setItem('Id', Id);
+        window.location.href = '/';
       }
-      reset();
     } catch (error) {
+      setErrorMessage("Ocorreu um erro ao tentar fazer login. Por favor, tente novamente.");
       console.error("Falha na requisição:", error);
     }
     reset();
   };
 
-
   return (
-    <div className="grid grid-cols-2 h-screen bg-n-8">
-      <div>
-        <div className="py-10 px-20">
-          <img src={brainwave} width={190} height={40} alt="CyberShield" />
-          <main className="flex flex-col gap-10 w-full max-w-[384px]:">
-            <header className="flex flex-col gap-4 w-full max-w-[400px]">
-              <h1 className="font-sans font-extrabold text-4xl text-n-1">
-                Acesse nosso jogo
-              </h1>
-              <p className="font-sans font-normal text-base text-n-2">
-                Faca login ou registre-se para comecar a jogar
-              </p>
-            </header>
-            <form
-              className="flex flex-col gap-4"
-              onSubmit={handleSubmit(handleLoginSubmit)}
-            >
-              <div className="flex flex-col gap-2">
-                <label
-                  className="font-sans  font-semibold text-sm text-n-1"
-                  htmlFor="E-mail"
-                >
-                  E-mail
-                </label>
-                <input
-                  className={clsx(
-                    "px-4 py-3 bg-white text-sm text-gray-800 leading-5 border border-gray-200 rounded placeholder:text-black outline-none focus:border-purple-900",
-                    {
-                      "border-red": errors.senha,
-                      "focus:border-red-500": errors.email,
-                    }
-                  )}
-                  type="text"
-                  id="email"
-                  placeholder="Digite seu e-mail"
-                  {...register("email")}
-                />
-                {errors.email && (
-                  <span className="text-red-500 text-sm">
-                    {" "}
-                    {errors.email?.message}{" "}
-                  </span>
-                )}
+    <div className="grid grid-cols-1 md:grid-cols-2 h-screen bg-n-8">
+      <div className="flex flex-col justify-center px-4 md:px-20 py-10">
+        <img src={brainwave} width={190} height={40} alt="CyberShield" />
+        <main className="flex flex-col gap-6 w-full max-w-md mx-auto">
+          <header className="flex flex-col gap-4 w-full">
+            <h1 className="font-sans font-extrabold text-2xl md:text-4xl text-n-1">
+              Acesse nosso jogo
+            </h1>
+            <p className="font-sans font-normal text-sm md:text-base text-n-2">
+              Faça login ou registre-se para começar a jogar
+            </p>
+          </header>
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit(handleLoginSubmit)}>
+            {errorMessage && (
+              <div className="bg-red-100 text-red-500 border border-red-300 p-4 rounded mb-4">
+                {errorMessage}
               </div>
-              <div className="flex  flex-col gap-2 relative">
-                <label
-                  className="flex font-sans justify-between font-semibold text-sm text-n-1"
-                  htmlFor="Senha"
-                >
-                  Senha
-                  <a
-                    className="text-purple-300 hover:text-purple-500 hover:underline"
-                    href="#"
-                  >
-                    Esqueceu sua senha?
-                  </a>
-                </label>
-                <input
-                  className={clsx(
-                    "px-4 py-3 bg-white text-sm text-gray-800 leading-5 border border-gray-200 rounded placeholder:text-black outline-none focus:border-purple-900",
-                    {
-                      "border-red": errors.email,
-                      "focus:border-red-500": errors.senha,
-                    }
-                  )}
-                  type={inputPasswordType}
-                  id="senha"
-                  placeholder="Digite sua senha"
-                  {...register("senha")}
-                />
-                <button
-                  className="absolute right-4 top-11 text-gray-400"
-                  type="button"
-                  onClick={() => handleTogglePasswordType(inputPasswordType)}
-                >
-                  {inputPasswordType === "password" ? <EyeSlash /> : <Eye />}
-                </button>
-                {errors.senha && (
-                  <span className="text-red-500 text-sm">
-                    {" "}
-                    {errors.senha?.message}{" "}
-                  </span>
+            )}
+            <div className="flex flex-col gap-2">
+              <label className="font-sans font-semibold text-sm text-n-1" htmlFor="email">
+                E-mail
+              </label>
+              <input
+                className={clsx(
+                  "px-4 py-3 bg-white text-sm text-gray-800 leading-5 border border-gray-200 rounded placeholder:text-black outline-none focus:border-purple-900",
+                  {
+                    "border-red-500": errors.email,
+                    "focus:border-red-500": errors.email,
+                  }
                 )}
-              </div>
-              <footer className="flex flex-col gap-8 ">
-                <button className="bg-purple-950 text-white font-sans font-bold py-4   rounded  hover:bg-purple-950 hover:ring-2 hover:ring-purple-900 focus:ring-2 focus:ring-purple-900">
-                  Entrar
-                </button>
-                <span className="text-n-1 ">
-                  Ainda nao tem uma conta?
-                  <a
-                    className="text-purple-300 hover:text-purple-500 hover:underline ml-2"
-                    href="/register"
-                  >
-                    Inscreva-se
-                  </a>
-                </span>
-              </footer>
-            </form>
-          </main>
-        </div>
+                type="text"
+                id="email"
+                placeholder="Digite seu e-mail"
+                {...register("email")}
+              />
+              {errors.email && (
+                <span className="text-red-500 text-sm">{errors.email?.message}</span>
+              )}
+            </div>
+            <div className="flex flex-col gap-2 relative">
+              <label className="flex font-sans justify-between font-semibold text-sm text-n-1" htmlFor="senha">
+                Senha
+                <a className="text-purple-300 hover:text-purple-500 hover:underline" href="#">
+                  Esqueceu sua senha?
+                </a>
+              </label>
+              <input
+                className={clsx(
+                  "px-4 py-3 bg-white text-sm text-gray-800 leading-5 border border-gray-200 rounded placeholder:text-black outline-none focus:border-purple-900",
+                  {
+                    "border-red-500": errors.senha,
+                    "focus:border-red-500": errors.senha,
+                  }
+                )}
+                type={inputPasswordType}
+                id="senha"
+                placeholder="Digite sua senha"
+                {...register("senha")}
+              />
+              <button
+                className="absolute right-4 top-11 text-gray-400"
+                type="button"
+                onClick={() => handleTogglePasswordType(inputPasswordType)}
+              >
+                {inputPasswordType === "password" ? <EyeSlash /> : <Eye />}
+              </button>
+              {errors.senha && (
+                <span className="text-red-500 text-sm">{errors.senha?.message}</span>
+              )}
+            </div>
+            <footer className="flex flex-col gap-6">
+              <button className="bg-purple-950 text-white font-sans font-bold py-3 rounded hover:bg-purple-900 hover:ring-2 hover:ring-purple-900 focus:ring-2 focus:ring-purple-900">
+                Entrar
+              </button>
+              <span className="text-n-1">
+                Ainda não tem uma conta?
+                <a className="text-purple-300 hover:text-purple-500 hover:underline ml-2" href="/register">
+                  Inscreva-se
+                </a>
+              </span>
+            </footer>
+          </form>
+        </main>
       </div>
-      <div className="bg-img-purple bg-cover bg-no-repeat"></div>
+      <div className="hidden md:block bg-img-purple bg-cover bg-no-repeat"></div>
     </div>
   );
 };
